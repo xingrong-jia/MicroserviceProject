47b6f8765160a46f37bd1c3a243cde1e380f3282
Luke.Deng <766718726@qq.com>
2020-01-10 14:59:22 +0800

    jiaxingrong

---
+++ jiaxingrong/guns/guns-api/src/main/java/com/stylefeng/guns/rest/model/Result.java
@@ -0,0 +1,58 @@
+package com.stylefeng.guns.rest.model;
+
+import lombok.Data;
+
+/**
+ * @author Xingrong.Jia
+ * @USER forever
+ * @PROJECT_NAME: guns
+ * @date 2020-01-07 21:02
+ */
+@Data
+public class Result<T> {
+
+    private Integer status;
+
+    private String msg;
+
+    private T data;
+
+    private String imgPre;
+
+    public Result(Integer status, T data) {
+        this.status = status;
+        this.data = data;
+    }
+
+    public static Result ok(String msg) {
+        return new Result(0, msg);
+    }
+
+    public static Result ok(Object o) {
+        return new Result(0, o);
+    }
+
+    public static Result ok(String imgPre, Object o) {
+        return new Result(0, o, imgPre);
+    }
+
+    public Result(Integer status, T data, String imgPre) {
+        this.status = status;
+        this.data = data;
+        this.imgPre = imgPre;
+    }
+
+    public Result(Integer status, String msg, T data) {
+        this.status = status;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    public Result(Integer status, String msg) {
+        this.status = status;
+        this.msg = msg;
+    }
+
+    public Result() {
+    }
+}
---
+++ jiaxingrong/guns/guns-cinema/src/main/java/com/stylefeng/guns/rest/common/persistence/dao/MtimeFieldTMapper.java
@@ -0,0 +1,16 @@
+package com.stylefeng.guns.rest.common.persistence.dao;
+
+import com.stylefeng.guns.rest.common.persistence.model.MtimeFieldT;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 放映场次表 Mapper 接口
+ * </p>
+ *
+ * @author stylefeng
+ * @since 2020-01-10
+ */
+public interface MtimeFieldTMapper extends BaseMapper<MtimeFieldT> {
+
+}
